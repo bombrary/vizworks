872aaae0e375b8b2e80374c8569cb31ad711f3da
bombrary <bombra108@gmail.com>
2019-08-31 09:59:20 +0900

    Added testcase.

--- SegTree/RMQ/script.js
+++ SegTree/RMQ/script.js
@@ -18,6 +18,9 @@ const startButton = menu.append('input')
 const stopButton = menu.append('input')
   .attr('type', 'button')
   .attr('value', 'stop');
+const testcaseButton = menu.append('input')
+  .attr('type', 'button')
+  .attr('value', 'testcase');
 const queryInfo = menu.append('div')
 const queryName = queryInfo.append('p');
 const queryArgs = queryInfo.append('p');
@@ -70,6 +73,30 @@ genButton.on('click', () => {
   update(rmqFormatter(st));
 });
 
+
+let itr_step;
+itrButton.on('click', () => {
+  if (query_step === undefined || itr_step === undefined) return;
+  if (itr_step === st.history.length) itr_step = 0;
+  iterateQuery();
+});
+
+let query_timer;
+const timer_interval = 50;
+const timerStart = () => {
+  if (query_step === undefined || itr_step === undefined) return;
+  itr_step = 0;
+  query_timer = d3.interval((elapsed) => {
+    if (itr_step === st.history.length - 1) query_timer.stop();
+    iterateQuery();
+  }, timer_interval);
+};
+startButton.on('click', timerStart);
+stopButton.on('click', () => {
+  if (query_step === undefined || itr_step === undefined) return;
+  query_timer.stop();
+});
+
 let query_step;
 nextQueryButton.on('click', () => {
   if (query_step === undefined) return;
@@ -88,28 +115,7 @@ nextQueryButton.on('click', () => {
     update(rmqFormatter(st.history[0], x, y + 1));
   }
   itr_step = 1;
-});
-
-let itr_step;
-itrButton.on('click', () => {
-  if (query_step === undefined || itr_step === undefined) return;
-  if (itr_step === st.history.length) itr_step = 0;
-  iterateQuery();
-});
-
-let query_timer;
-const timer_interval = 50;
-startButton.on('click', () => {
-  if (query_step === undefined || itr_step === undefined) return;
-  itr_step = 0;
-  query_timer = d3.interval((elapsed) => {
-    if (itr_step === st.history.length - 1) query_timer.stop();
-    iterateQuery();
-  }, timer_interval);
-});
-stopButton.on('click', () => {
-  if (query_step === undefined || itr_step === undefined) return;
-  query_timer.stop();
+  timerStart();
 });
 
 function iterateQuery() {
@@ -119,3 +125,60 @@ function iterateQuery() {
   update(rmqFormatter(st.history[itr_step], l, r));
   itr_step++;
 }
+
+const testcase = 
+"10 50\n" +
+"0 2 43\n" +
+"0 8 0\n" +
+"1 6 6\n" +
+"0 1 11\n" +
+"0 8 5\n" +
+"0 6 30\n" +
+"0 7 32\n" +
+"0 8 22\n" +
+"1 3 9\n" +
+"1 4 9\n" +
+"0 0 27\n" +
+"1 0 5\n" +
+"1 0 2\n" +
+"1 1 3\n" +
+"1 6 6\n" +
+"0 7 29\n" +
+"0 0 41\n" +
+"1 4 5\n" +
+"0 7 30\n" +
+"1 3 4\n" +
+"1 0 9\n" +
+"1 2 6\n" +
+"1 0 3\n" +
+"0 5 19\n" +
+"0 7 49\n" +
+"0 4 47\n" +
+"1 6 9\n" +
+"1 3 5\n" +
+"0 9 6\n" +
+"1 2 5\n" +
+"0 1 38\n" +
+"1 0 7\n" +
+"0 3 6\n" +
+"0 2 27\n" +
+"1 2 8\n" +
+"0 0 10\n" +
+"1 1 1\n" +
+"1 4 7\n" +
+"0 8 25\n" +
+"0 7 18\n" +
+"0 3 37\n" +
+"0 0 45\n" +
+"1 2 3\n" +
+"1 3 7\n" +
+"0 0 30\n" +
+"0 6 28\n" +
+"0 8 47\n" +
+"0 8 38\n" +
+"0 8 43\n" +
+"1 1 6";
+
+testcaseButton.on('click', () => {
+  textarea.property('value', testcase);
+});
